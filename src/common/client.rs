18160cdd613f4d3f32200e359b474b0875db88aa
Philipp Meier <philipp.meier@gmx.at>
2022-11-05 19:12:38 +0100

    Performance improvements.

--- src/common/client.rs
+++ src/common/client.rs
@@ -1,19 +1,27 @@
+use reqwest::header::{ACCEPT_LANGUAGE, AUTHORIZATION};
 use serde::de::DeserializeOwned;
-use reqwest::header::{AUTHORIZATION, ACCEPT_LANGUAGE};
 
 pub struct Gw2Client {
     api_key: String,
     lang: String,
-    req_client: reqwest::Client
+    req_client: reqwest::Client,
 }
 
 impl Gw2Client {
     pub fn new(api_key: String, lang: String) -> Gw2Client {
-        Self { api_key, lang, req_client: reqwest::Client::new() }
+        Self {
+            api_key,
+            lang,
+            req_client: reqwest::Client::new(),
+        }
     }
 
-    pub async fn request<T>(&self, endpoint: &str) -> Result<T, Gw2ApiError> where T: DeserializeOwned {
-        let response = self.req_client
+    pub async fn request<T>(&self, endpoint: &str) -> Result<T, Gw2ApiError>
+    where
+        T: DeserializeOwned,
+    {
+        let response = self
+            .req_client
             .get(format!("https://api.guildwars2.com/{endpoint}"))
             .header(AUTHORIZATION, format!("Bearer {}", self.api_key))
             .header(ACCEPT_LANGUAGE, self.lang.to_string())
@@ -21,30 +29,36 @@ impl Gw2Client {
             .await?;
 
         match response.status() {
-            reqwest::StatusCode::OK => {
-                match response.json::<T>().await {
-                    Ok(result) => Ok(result),
-                    Err(_) => Err(Gw2ApiError::new("The response didn't match the expected shape.")),
-                }
+            reqwest::StatusCode::OK => match response.json::<T>().await {
+                Ok(result) => Ok(result),
+                Err(_) => Err(Gw2ApiError::new(
+                    "The response didn't match the expected shape.",
+                )),
             },
             reqwest::StatusCode::UNAUTHORIZED => Err(Gw2ApiError::new("Unauthorized.")),
-            _ => Err(Gw2ApiError::new("Something unexpected happened."))
+            _ => Err(Gw2ApiError::new("Something unexpected happened.")),
         }
     }
 }
 
+#[derive(Debug)]
 pub struct Gw2ApiError {
     pub error: String,
 }
 
 impl Gw2ApiError {
     pub fn new(msg: &str) -> Gw2ApiError {
-        Self { error: msg.to_string() }
+        Self {
+            error: msg.to_string(),
+        }
     }
 }
 
 impl From<reqwest::Error> for Gw2ApiError {
     fn from(err: reqwest::Error) -> Self {
-        Self { error: format!("{:?}", err) }
+        Self {
+            error: format!("{:?}", err),
+        }
     }
 }
+
